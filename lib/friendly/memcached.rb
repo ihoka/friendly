2354204871d958e866bcefa5570a2bba627f43bd
James Golick <james@giraffesoft.ca>
2010-02-07 20:42:22 -0800

    add benchmarking to Friendly::Memcached

--- lib/friendly/memcached.rb
+++ lib/friendly/memcached.rb
@@ -1,5 +1,11 @@
+require 'friendly/logging'
+require 'friendly/benchmarking'
+
 module Friendly
   class Memcached
+    include Logging
+    include Benchmarking
+
     attr_reader :cache
 
     def initialize(cache)
@@ -7,26 +13,36 @@ module Friendly
     end
 
     def set(key, value)
-      @cache.set(key, value)
+      benchmark("[CACHE] Set #{key} to #{value} in :time.") do
+        @cache.set(key, value)
+      end
     end
 
     def get(key)
-      @cache.get(key)
+      benchmark("[CACHE] Got #{key} in :time.") do
+        @cache.get(key)
+      end
     rescue ::Memcached::NotFound
       if block_given?
-        miss(key) { yield }
+        benchmark("[CACHE] Missed #{key} in :time.") do
+          miss(key) { yield }
+        end
       end
     end
 
     def multiget(keys)
       return {} if keys.empty?
 
-      hits         = @cache.get(keys)
+      hits = benchmark("[CACHE] Multiget #{keys.join(",")} in :time.") do
+        @cache.get(keys)
+      end
       missing_keys = keys - hits.keys
 
       if !missing_keys.empty? && block_given?
         missing_keys.each do |missing_key|
-          hits.merge!(missing_key => miss(missing_key) { yield(missing_key) })
+          benchmark("[CACHE] Multiget missed #{missing_key} in :time.") do
+            hits.merge!(missing_key => miss(missing_key) { yield(missing_key) })
+          end
         end
       end
 
@@ -34,7 +50,9 @@ module Friendly
     end
 
     def delete(key)
-      cache.delete(key)
+      benchmark("[CACHE] Deleted #{key} in :time.") do
+        cache.delete(key)
+      end
     rescue ::Memcached::NotFound
     end
 
